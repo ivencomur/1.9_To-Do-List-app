7a1a9a192b20f617de45146ad8dfe3ea8851ff0a
ivencomur <ivancortes@hotmail.com>
2025-02-01 17:39:19 -0500

    FINAL VERSION

--- js/script.js
+++ js/script.js
@@ -1,52 +1,52 @@
 function newItem() {
-  let inputValue = $('#input').val();
-  
-  if (inputValue === '') {
-      alert("You must write something!");
-      return;
-  }
+    let inputValue = $('#input').val();
+    
+    if (inputValue === '') {
+        alert("Please, write something!");
+        return;
+    }
 
-  let li = $('<li></li>');
-  li.append(inputValue);
+    let li = $('<li class="ui-state-default"></li>');
+    
+    let dragHandle = $('<span class="ui-icon ui-icon-arrowthick-2-n-s"></span>');
+    li.append(dragHandle);
+    
+    li.append(inputValue);
 
-  let checkbox = $('<input type="checkbox"/>');
-  li.append(checkbox);
-  
-  let checkbox = $('<input type="checkbox" />');
-  checkbox.addEventListener('click', function(){
-    checkbox.addClass('completed')
-})
-li.append(checkbox);
+    let checkbox = $('<input type="checkbox"/>');
+    checkbox.on('click', function() {
+        li.toggleClass('completed');
+    });
+    li.append(checkbox);
 
-  let crossOutButton = $('<button></button>');
-  crossOutButton.append(document.createTextNode('X'));
-  li.append(crossOutButton);
+    let crossOutButton = $('<button></button>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
 
-  $('#list').append(li);
-  $('#input').val('');
+    $('#list').append(li);
+    
+    $('#input').val('');
 
-  li.on("dblclick", function() {
-      li.toggleClass("strike");
-  });
+    li.on("dblclick", function() {
+        li.toggleClass("strike");
+    });
 
-  crossOutButton.on("click", function() {
-      li.addClass("delete");
-  });
-
-  $('#list').sorteable();
+    crossOutButton.on("click", function() {
+        li.addClass("delete");
+    });
 }
 
 $(document).ready(function() {
-  $('#list').sorteable();
-  
-  $('#input').keydown(function(event) {
-      if (event.keyCode === 13) {
-          event.preventDefault();
-          newItem();
-      }
-  });
+    $('#list').sortable();
+    
+    $('#input').keydown(function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            newItem();
+        }
+    });
 
-  $('#button').on('click', function() {
-      newItem();
-  });
+    $('#button').on('click', function() {
+        newItem();
+    });
 });
